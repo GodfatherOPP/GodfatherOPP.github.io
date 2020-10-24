d81d49c2340d1b14c42ec15b98279f7cde0f9286
godfatheropp <harshsinghrathorr@gmail.com>
2020-10-24 16:08:19 +0530

    Game edited

--- Game/tic-tac-toe/main.js
+++ Game/tic-tac-toe/main.js
@@ -7,26 +7,26 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // combinations at which a person wins
   const wincombination = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
     [1, 4, 7],
-    [2, 5, 8],
-    [3, 6, 9],
-    [1, 5, 9],
-    [7, 5, 3],
+    [3, 5, 8],
+    [0, 4, 8],
+    [6, 4, 2],
   ];
   // array on which value is filled after click of each box
-  blockvaluecombination = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',];
+  blockvaluecombination = ['', '', '', '', '', '', '', '', ''];
   var currentPlayer = "X";
-
+  var winnerIs = "";
 
   //function to start game
   function startGame(event) {
     reversePlayer();
 //it print value X or O on blocks
     function reversePlayer() {
-      if (event.target.innerHTML != '') {
+      if (event.target.innerHTML != '' || winnerIs!= '') {
         return;
       }
       currentPlayer = currentPlayer === "X" ? "O" : "X";
@@ -36,11 +36,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 //get value filled inside the block and position of the block
-    let blockvalue = event.target.innerHTML;
+    // let blockvalue = event.target.innerHTML;
     let blockposition = event.target.getAttribute("id");
  
     // fill value of block on array
-    blockvaluecombination[blockposition] = blockvalue;
+    blockvaluecombination[blockposition] = currentPlayer;
     win()
   }
 
@@ -58,24 +58,26 @@ document.addEventListener("DOMContentLoaded", function () {
     });
     console.log("blockFilled " +  blockFilled);
     return blockFilled.length;
-    // console.log(blockFilled.length);
   }
 
   function winner() {
-    let winnerIs;
-    for (i = 0; i < wincombination - 1; i++) {
+
+    for (i = 0; i < wincombination.length - 1; i++) {
       let element = wincombination[i];
       let a = blockvaluecombination[element[0]];
       let b = blockvaluecombination[element[1]];
       let c = blockvaluecombination[element[2]];
+
       if (a === '' || b === '' || c === '') {
         continue;
       }
       if (a === b && b === c) {
-        winnerIs = a;
-        break;
+        winnerIs = b;
+        document.getElementById("over").innerHTML = winnerIs + " has won the game";
+     document.getElementById("image").style.display = "block";
+         break;
+        }
       }
     }
-    document.getElementById("over").innerHTML = winnerIs + " has won the game"
-  }
+    // document.getElementById("game").removeEventListener("click", startGame); 
 });
