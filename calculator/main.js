05e365f82c3fac18d15f7818fe0c3c891c343bb1
godfatheropp <harshsinghrathorr@gmail.com>
2020-11-18 16:20:04 +0530

    calculator updated

--- calculator/main.js
+++ calculator/main.js
@@ -1,54 +1,86 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", ()=>{
+ var tempNumber = "";
+ var screen ;
+ var Value = "";
+ var tempOperator= "";
+ var operation = [];
+var number = [];
 
-    var operator = "";
-    var numberOne = "";
-    var numberTwo = "";
-    var screen;
-    var answer;
-    document.getElementById("number").addEventListener("click", (event) => {
-
-        if (operator === "") {
-            numberOne += event.target.getAttribute("value");
-            document.getElementById("screen").innerHTML = numberOne;
-            console.log("numberOne: " + numberOne)
-        } else {
-            numberTwo += event.target.getAttribute("value");
-            document.getElementById("screen").innerHTML =numberOne + operator + numberTwo;
-            console.log("numberTwo: " + numberTwo)
+    document.getElementById("number").addEventListener("click", (event)=> {
+      if(document.getElementById("screen").innerHTML == 0){
+        document.getElementById("screen").innerHTML = "";
+      }
+          Value = event.target.getAttribute("data-value");
+        tempNumber += Value;
+        screen = document.getElementById("screen").innerHTML += Value;
+        console.log("tempNumber: " + tempNumber);
+        if(tempOperator!= ""){
+            operation.push(tempOperator);
+            tempOperator="";
         }
-
+        console.log("operation: " + operation);
     });
-
-    document.getElementById("operation").addEventListener("click", (event) => {
-        operator = event.target.getAttribute("value");
-        screen = document.getElementById("screen").innerHTML += operator;
-        console.log("operator: " + operator);
-
-    });
-
-    document.getElementById("equal").addEventListener("click", (event) => {
-        switch (operator) {
-            case "+":
-                answer = document.getElementById("screen").innerHTML = Number(numberOne) + Number(numberTwo);
-                break;
-            case "-":
-                answer = document.getElementById("screen").innerHTML = Number(numberOne) - Number(numberTwo);
-                break;
-            case "*":
-                answer = document.getElementById("screen").innerHTML = Number(numberOne) * Number(numberTwo);
-                break;
-            case "/":
-                answer = document.getElementById("screen").innerHTML = Number(numberOne) / Number(numberTwo);
-                break;
-        }
-        console.log("Answer: " + answer);
-
-    })
-    // document.getElementById("Clear").addEventListener("click", function(event){
-    //     document.getElementById("screen").innerHTML = " afs";
+    
+    document.getElementById("operation").addEventListener("click", (event)=> {
+        number.push(tempNumber);
+        tempNumber= "";
+        console.log("tempNumber: " + tempNumber);
+        console.log("number: " + number);
       
-    //   });
-});
-function clearscreen(){
-    location.reload();
-}
+      console.log("tempOperator: ${tempOperator}");
+
+      //solve first written equation if two operator clicked at same time 
+      if(operation.length == 1 ){
+        let currentoperator = operation[0];
+        if (currentoperator === '+') {
+          result = Number(number[0]) + Number(number[1])
+        } else if (currentoperator === '-') {
+          result = Number(number[0]) - Number(number[1])
+        } else if (currentoperator === '*') {
+          result = Number(number[0]) * Number(number[1])
+        } else if (currentoperator === '/') {
+          result = Number(number[0]) / Number(number[1])
+        }
+        number = [];
+        number.push(result);
+        operation = [];
+        screen = document.getElementById("screen").innerHTML = result;
+      }
+      //End
+            // do not print operator in series 
+            if(tempOperator== "" ){
+      Value = event.target.getAttribute("data-value");
+      tempOperator = Value;
+      screen = document.getElementById("screen").innerHTML += tempOperator;
+            }
+           // end
+  });
+  // ON the click of equal 
+  document.getElementById("equal").addEventListener("click", (event)=> {
+       if(tempNumber!= ""){
+           number.push(tempNumber);
+      }
+        let currentoperator = operation[0];
+        if (currentoperator === '+') {
+          result = Number(number[0]) + Number(number[1])
+        } else if (currentoperator === '-') {
+          result = Number(number[0]) - Number(number[1])
+        } else if (currentoperator === '*') {
+          result = Number(number[0]) * Number(number[1])
+        } else if (currentoperator === '/') {
+          result = Number(number[0]) / Number(number[1])
+        }
+        document.getElementById("screen").innerHTML = result;
+        return result;
+      })
+      document.getElementById("Clear").addEventListener("click", (event)=> {
+        document.getElementById("screen").innerHTML = 0;
+         tempNumber = "";
+         screen ;
+         Value = "";
+         tempOperator= "";
+         operation = [];
+         number = [];
+         console.clear();
+      })
+    })
